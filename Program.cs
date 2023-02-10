ecce10419d7cbbc08ce0b4cec493858e6fc20005
Daria <darya.piankova@yandex.ru>
2023-02-10 20:13:22 +0500

    task23

--- Program.cs
+++ Program.cs
@@ -1,24 +1,8 @@
 ﻿Console.Clear();
+Console.WriteLine("Введите число n");
+int n = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите координаты точки A. Введите x1");
-double x1 = Convert.ToInt32(Console.ReadLine());
+for (int i=1; i<=n; i++ ){
+    Console.Write($"{i*i*i} ");
+}
 
-Console.WriteLine("Введите y1");
-double y1 = Convert.ToInt32(Console.ReadLine());
-
-Console.WriteLine("Введите z1");
-double z1 = Convert.ToInt32(Console.ReadLine());
-
-
-Console.WriteLine("Введите координаты точки Б. Введите x2");
-double x2 = Convert.ToInt32(Console.ReadLine());
-
-Console.WriteLine("Введите y2");
-double y2 = Convert.ToInt32(Console.ReadLine());
-
-Console.WriteLine("Введите z2");
-double z2 = Convert.ToInt32(Console.ReadLine());
-
-
-double result=Math.Sqrt(Math.Pow(x1-x2, 2)+Math.Pow(y1-y2, 2)+Math.Pow(z1-z2, 2));
-Console.WriteLine(result);
