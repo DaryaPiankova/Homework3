3d6415fd479310f30fb5b6166b914ddfe48914ba
Daria <darya.piankova@yandex.ru>
2023-02-10 20:32:33 +0500

    doptask

--- Program.cs
+++ Program.cs
@@ -1,8 +1,12 @@
 ﻿Console.Clear();
-Console.WriteLine("Введите число n");
+Console.WriteLine("Введите кол-во кустов на грядке");
 int n = Convert.ToInt32(Console.ReadLine());
-
-for (int i=1; i<=n; i++ ){
-    Console.Write($"{i*i*i} ");
+int maxSum=0;
+for (int i=1; i<=n-2; i++){
+    int sum=i+(i+1)+(i+2);
+    if(sum>maxSum){
+        maxSum=sum;
+    }
 }
 
+Console.WriteLine(maxSum);
