0d8b761091f49bdcfb052aeeb18ba176aadab592
Daria <darya.piankova@yandex.ru>
2023-02-10 20:01:18 +0500

    task19

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Введите пятизначное число");
+string a=Console.ReadLine();
+int count=0;
+for(int i=0; i<(a.Length/2); i++){
+    if(a[i]!=a[a.Length-i-1]){
+       count++;        
+    }
+}
+if(count==0){
+    Console.WriteLine("Да");
+}else{
+    Console.WriteLine("Нет");
+}
